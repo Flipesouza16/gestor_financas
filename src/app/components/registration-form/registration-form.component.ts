24e1db2129b6273df27e43f06d91f6904e0679df
Filipe Souza <filipe.souza1602@gmail.com>
2021-11-17 07:45:32 -0300

    feat: add data bind in registration form #1

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
+type PayloadRegistrationForm = {
+  purchaseTitle: string;
+  purchaseValue: string;
+  personWhoIsBuying: string;
+  purchaseInstallments: number;
+};
+
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
 })
 export class RegistrationFormComponent implements OnInit {
+  payloadRegistrationForm: PayloadRegistrationForm = {
+    personWhoIsBuying: '',
+    purchaseInstallments: 1,
+    purchaseTitle: '',
+    purchaseValue: '',
+  };
 
   constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
+
+  addNewPurchase() {
+    console.log('payloadRegistrationForm: ', this.payloadRegistrationForm);
+  }
 
   dismiss() {
     this.modalCtrl.dismiss();
